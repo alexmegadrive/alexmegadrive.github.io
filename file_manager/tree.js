cc2217897888347eab7094335dc2b95219401ebb
Alexander Mihailov <53320300+alexmegadrive@users.noreply.github.com>
2022-07-25 20:54:58 +0500

    added some folders

--- file_manager/tree.js
+++ file_manager/tree.js
@@ -3,7 +3,7 @@
     {
         id: 1,
         parent: 0,
-        name: 'File2.txt',
+        name: 'styles.css',
         isFolder: false,
         content: ".header {\n    background: linear-gradient(90.01deg, #2A8BF2 0.01%, #0A6CD6 99.99%);\n    height: 70px;\n    display: flex;\n    align-items: center;\n}\n\n.header__wrapper {\n    max-width: 1560px;\n    margin-left:350px;\n    width: 100%;\n    padding: 20px 0;\n    display: flex;\n    justify-content: space-between;\n    align-items: center;\n}\n\n.header__avatar-icon {\n    width: 60px;\n    height: 60px;\n    border-radius: 50%;\n    margin-right: 20px;\n}\n'",
         // content: "let treeObjRef = document.querySelector('.ChatMainList_Group') \nlet newElement = document.createElement(div);",
@@ -13,10 +13,18 @@
     {
         id: 7,
         parent: 0,
-        name: 'File3.doc',
+        name: 'some_File3.doc',
         isFolder: false,
         content: "File3 : 333 333 333\nlet newElement = document.createElement(div);\n <div class=header__icon-settings_delete></div>",
         description: "test desc file 3333 рабочие материалы",
+        isActive: false
+    },
+	{
+        id: 105,
+        parent: 0,
+        name: 'template4.doc',
+        isFolder: false,
+        content: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
         isActive: false
     },
     {
@@ -29,15 +37,40 @@
         isFolderExpanded: false
 
     },
+	
     {
-        id: 3,
+        id: 106,
         parent: 0,
-        name: 'folder5',
+        name: 'project6',
         description: "это папка Folder#5",
         isFolder: true,
         children: [],
         isActive: false,
         isFolderExpanded: false
+    },
+	{
+        id: 107,
+        parent: 106,
+        name: 'project6-text.doc',
+        isFolder: false,
+        content: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+        isActive: false
+    },
+	{
+        id: 108,
+        parent: 106,
+        name: 'project6-documents.doc',
+        isFolder: false,
+        content: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+        isActive: false
+    },
+	{
+        id: 109,
+        parent: 106,
+        name: 'project6-notes.doc',
+        isFolder: false,
+        content: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+        isActive: false
     },
     {
         id: 4,
